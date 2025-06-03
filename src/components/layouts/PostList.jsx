27c1ae33705f99fc9abc35de8b69f1a119af94db
yuricifiello <yuricifiello@gmail.com>
2025-06-03 17:15:43 +0200

    add axios

--- src/components/layouts/PostList.jsx
+++ src/components/layouts/PostList.jsx
@@ -1,8 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 export default function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/posts")
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div>
-      <h1>Lista dei post</h1>
-      <p>Qui vedrai i post del nostro blog.</p>
+      <h1>Lista Post</h1>
+      <ul className="list-group">
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            {post.title}
+            <Link to={`/posts/${post.id}`} className="btn btn-primary btn-sm">
+              Dettagli
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
